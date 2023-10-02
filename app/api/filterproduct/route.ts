c7868f0042443ddd58300a6c19ce2db12754762b
satender maurya <satendermaurya@satenders-MacBook-Air.local>
2023-10-02 12:26:12 +0530

    build

--- app/api/filterproduct/route.ts
+++ app/api/filterproduct/route.ts
@@ -1,49 +1,33 @@
-import prisma from "@/app/prismadb"
-import { NextResponse } from "next/server"
+import prisma from "@/app/prismadb";
+import { NextResponse } from "next/server";
+export const dynamic = 'auto'
+// 'auto' | 'force-dynamic' | 'error' | 'force-static'
 
+export async function GET(request: Request) {
+  try {
+    const searchParams = new URLSearchParams(request.credentials);
 
-export async function GET(request: Request){
-    try{
-        const searchParams = new URLSearchParams(request.url.split('?')[1])
-        const categories = searchParams.getAll('categories[]')
-        const colors = searchParams.getAll('colors[]')
-        let sizes  = searchParams.getAll('size[]')
-        const minPriceStr = searchParams.get('price[min]')
-        const maxPriceStr = searchParams.get('price[max]')
+    const categories = searchParams.getAll("categories[]");
+    const colors = searchParams.getAll("colors[]");
+    const sizes = searchParams.getAll("size[]");
 
-        const minPrice = minPriceStr ? parseInt(minPriceStr) : undefined
-        const maxPrice = maxPriceStr ? parseInt(maxPriceStr) : undefined
+    const minPrice = parseInt(searchParams.get("price[min]") || "0");
+    const maxPrice = parseInt(searchParams.get("price[max]") || "100000");
 
-        const products = await prisma.product.findMany({
-            where:{
-                OR:[
-                    ...categories.map((category) => ({
-                        style:{
-                            contains:category
-                        }
-                    })),
-                    ...sizes.map((size) => ({
-                        size:{
-                            contains:size
-                        }
-                    })),
-                    ...colors.map((color) => ({
-                        color:{
-                            contains:color
-                        }
-                    })),
-                    {
-                        price:{
-                            gte:minPrice,
-                            lte:maxPrice
-                        }
-                    }
-                ]
-            }
-        })
-        return NextResponse.json(products)
-    }catch(error){
-        console.log("Error selecting product", error)
-        return NextResponse.error()
-    }
+    const products = await prisma.product.findMany({
+      where: {
+        OR: [
+          ...categories.map((category) => ({ style: { contains: category } })),
+          ...sizes.map((size) => ({ size: { contains: size } })),
+          ...colors.map((color) => ({ color: { contains: color } })),
+          { price: { gte: minPrice, lte: maxPrice } },
+        ],
+      },
+    });
+
+    return NextResponse.json({ message: "API route is working" });
+  } catch (error) {
+    console.error("Error selecting product", error);
+    return NextResponse.error();
+  }
 }
